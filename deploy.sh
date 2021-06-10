28f973d55d892fb1e19a3509293852c3dfc653f8
jsprbld-gen:ba6321d3d6bcf9ee17219f37827afb1aaaa60640 <jsprbld.gen@cisco.com>
2021-06-10 09:47:34 -0700

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t bipulh/fibonacci-client:latest -t bipulh/fibonacci-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t bipulh/fibonacci-server:latest -t bipulh/fibonacci-server:$GIT_SHA  -f ./server/Dockerfile ./server
+docker build -t bipulh/fibonacci-worker:latest -t bipulh/fibonacci-worker:$GIT_SHA  -f ./worker/Dockerfile ./worker
+
+docker push bipulh/fibonacci-client:latest
+docker push bipulh/fibonacci-server:latest
+docker push bipulh/fibonacci-worker:latest
+
+docker push bipulh/fibonacci-client:$GIT_SHA
+docker push bipulh/fibonacci-server:$GIT_SHA
+docker push bipulh/fibonacci-worker:$GIT_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=bipulh/fibonacci-server:$GIT_SHA
+kubectl set image deployments/client-deployment server=bipulh/fibonacci-client:$GIT_SHA
+kubectl set image deployments/worker-deployment server=bipulh/fibonacci-worker:$GIT_SHA
